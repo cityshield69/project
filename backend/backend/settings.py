fb51a1d9495c7506b54051601a956dcdddb7582b
Meet Shah <meetshah4063@gmail.com>
2025-06-30 23:28:15 +0530

    Deployment Commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&!3u=vt+x@-d2kxnqq=8&q9h#lky!(eieb4ky63%ig%3+=+a$6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Allow all hosts temporarily
 
 
 # Application definition
