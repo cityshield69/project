d9bd8ec5d39fcc9b741de752ada27c42bc927c53
Meet Shah <meetshah4063@gmail.com>
2025-07-01 09:41:35 +0530

    prod err

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,7 +74,9 @@ TEMPLATES = [
         },
     },
 ]
-TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'frontend')]
+TEMPLATES[0]['DIRS'] = [
+    os.path.join(BASE_DIR, 'frontend', 'build')
+]
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
@@ -126,7 +128,9 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 # STATICFILES_DIRS = [BASE_DIR / "frontend" / "static"]
 # STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # Default primary key field type
